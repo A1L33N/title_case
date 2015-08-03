65d97feffc95fb76cb81496fb8e73bfaf6e29d6b
Aileen Cacayorin & Jesse Zanavich <student@epicodus.com>
2015-08-03 14:22:09 -0700

    manage mixed case entries, capitalizes first word, does not capitalize prepositions

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,5 +1,14 @@
 class String
+  prepositions = ["a", "and", "the", "with", "of", "from", "is"]
   define_method(:title_case) do
-    self.capitalize!()
- end
+    split_title = self.split()
+    split_title.each() do |word|
+      word.capitalize!()
+      if prepositions.include?(word.downcase())
+        word.downcase!()
+      end
+      split_title[0].capitalize!()
+    end
+    split_title.join(" ")
+  end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,7 +2,28 @@ require('rspec')
 require('title_case')
 
 describe('String#title_case') do
+
+  it("manages mixed cased entries") do
+    expect(("heLLo WORLD").title_case()).to(eq("Hello World"))
+  end
+
   it("capitalizes the first letter of a word") do
     expect(("hamlet").title_case()).to(eq("Hamlet"))
+  end
+
+  it ("capitalizes the first letter of every word") do
+    expect(("ready player one").title_case()).to(eq("Ready Player One"))
+  end
+
+  it ("handles non-letter characters") do
+    expect(("100 days of solitude").title_case()).to(eq("100 Days of Solitude"))
+  end
+
+  it("does not capitalize prepositions") do
+    expect(("gone with the wind").title_case()).to(eq("Gone with the Wind"))
+  end
+
+  it("capitalizes designated words if they are the first word") do
+    expect(("a tale of two cities").title_case()).to(eq("A Tale of Two Cities"))
   end
 end
